dabe1fa781460cfe8ca6515d468a87bdb04e33d1
Nathan Drake <nathandrakedrums@gmail.com>
2022-09-13 18:27:53 -0500

    build mp4 functionality

--- nft.py
+++ nft.py
@@ -2,16 +2,17 @@
 # coding: utf-8
 
 # Import required libraries
-from PIL import Image
 import pandas as pd
 import numpy as np
 import time
 import os
 import random
 from progressbar import progressbar
+from moviepy.editor import *
 
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 # Import configuration file
@@ -20,110 +21,105 @@ from config import CONFIG
 
 # Parse the configuration file and make sure it's valid
 def parse_config():
-    
+
     # Input traits must be placed in the assets folder. Change this value if you want to name it something else.
-    assets_path = 'assets'
+    assets_path = "assets"
 
     # Loop through all layers defined in CONFIG
     for layer in CONFIG:
 
         # Go into assets/ to look for layer folders
-        layer_path = os.path.join(assets_path, layer['directory'])
-        
+        layer_path = os.path.join(assets_path, layer["directory"])
+
         # Get trait array in sorted order
-        traits = sorted([trait for trait in os.listdir(layer_path) if trait[0] != '.'])
+        traits = sorted([trait for trait in os.listdir(layer_path) if trait[0] != "."])
 
         # If layer is not required, add a None to the start of the traits array
-        if not layer['required']:
+        if not layer["required"]:
             traits = [None] + traits
-        
+
         # Generate final rarity weights
-        if layer['rarity_weights'] is None:
+        if layer["rarity_weights"] is None:
             rarities = [1 for x in traits]
-        elif layer['rarity_weights'] == 'random':
+        elif layer["rarity_weights"] == "random":
             rarities = [random.random() for x in traits]
-        elif type(layer['rarity_weights'] == 'list'):
-            assert len(traits) == len(layer['rarity_weights']), "Make sure you have the current number of rarity weights"
-            rarities = layer['rarity_weights']
+        elif type(layer["rarity_weights"] == "list"):
+            assert len(traits) == len(
+                layer["rarity_weights"]
+            ), "Make sure you have the current number of rarity weights"
+            rarities = layer["rarity_weights"]
         else:
             raise ValueError("Rarity weights is invalid")
-        
+
         rarities = get_weighted_rarities(rarities)
-        
+
         # Re-assign final values to main CONFIG
-        layer['rarity_weights'] = rarities
-        layer['cum_rarity_weights'] = np.cumsum(rarities)
-        layer['traits'] = traits
+        layer["rarity_weights"] = rarities
+        layer["cum_rarity_weights"] = np.cumsum(rarities)
+        layer["traits"] = traits
 
 
 # Weight rarities and return a numpy array that sums up to 1
 def get_weighted_rarities(arr):
-    return np.array(arr)/ sum(arr)
+    return np.array(arr) / sum(arr)
 
-# Generate a single image given an array of filepaths representing layers
-def generate_single_image(filepaths, output_filename=None):
-    
-    # Treat the first layer as the background
-    bg = Image.open(os.path.join('assets', filepaths[0]))
-    
-    
-    # Loop through layers 1 to n and stack them on top of another
-    for filepath in filepaths[1:]:
-        if filepath.endswith('.png'):
-            img = Image.open(os.path.join('assets', filepath))
-            bg.paste(img, (0,0), img)
-    
+
+# Generate a Deity NFT
+def generate_single_deity(filepaths, output_filename=None):
+
+    deity_clip = VideoFileClip(os.path.join("assets", filepaths[1]), True).set_position(
+        "center"
+    )
+    duration = deity_clip.duration
+    bg_clip = ImageClip(os.path.join("assets", filepaths[0])).set_duration(duration)
+    video = CompositeVideoClip([bg_clip, deity_clip])
+
     # Save the final image into desired location
     if output_filename is not None:
-        bg.save(output_filename)
+        video.write_videofile(output_filename)
     else:
         # If output filename is not specified, use timestamp to name the image and save it in output/single_images
-        if not os.path.exists(os.path.join('output', 'single_images')):
-            os.makedirs(os.path.join('output', 'single_images'))
-        bg.save(os.path.join('output', 'single_images', str(int(time.time())) + '.png'))
+        if not os.path.exists(os.path.join("output", "single_images")):
+            os.makedirs(os.path.join("output", "single_images"))
+        video.write_videofile(
+            os.path.join("output", "single_images", str(int(time.time())) + ".mp4")
+        )
 
 
 # Generate a single image with all possible traits
-# generate_single_image(['Background/green.png', 
-#                        'Body/brown.png', 
-#                        'Expressions/standard.png',
-#                        'Head Gear/std_crown.png',
-#                        'Shirt/blue_dot.png',
-#                        'Misc/pokeball.png',
-#                        'Hands/standard.png',
-#                        'Wristband/yellow.png'])
+# generate_single_deity(["Background/gradient_horizon-01.png", "Deity/00_ram_gold.mov"])
 
 
 # Get total number of distinct possible combinations
 def get_total_combinations():
-    
+
     total = 1
     for layer in CONFIG:
-        total = total * len(layer['traits'])
+        total = total * len(layer["traits"])
     return total
 
 
 # Select an index based on rarity weights
 def select_index(cum_rarities, rand):
-    
+
     cum_rarities = [0] + list(cum_rarities)
     for i in range(len(cum_rarities) - 1):
-        if rand >= cum_rarities[i] and rand <= cum_rarities[i+1]:
+        if rand >= cum_rarities[i] and rand <= cum_rarities[i + 1]:
             return i
-    
+
     # Should not reach here if everything works okay
     return None
 
 
 # Generate a set of traits given rarities
 def generate_trait_set_from_config():
-    
+
     trait_set = []
     trait_paths = []
-    
+
     for layer in CONFIG:
         # Extract list of traits and cumulative rarity weights
-        traits, cum_rarities = layer['traits'], layer['cum_rarity_weights']
+        traits, cum_rarities = layer["traits"], layer["cum_rarity_weights"]
 
         # Generate a random number
         rand_num = random.random()
@@ -136,53 +132,53 @@ def generate_trait_set_from_config():
 
         # Add trait path to trait paths if the trait has been selected
         if traits[idx] is not None:
-            trait_path = os.path.join(layer['directory'], traits[idx])
+            trait_path = os.path.join(layer["directory"], traits[idx])
             trait_paths.append(trait_path)
-        
+
     return trait_set, trait_paths
 
 
 # Generate the image set. Don't change drop_dup
 def generate_images(edition, count, drop_dup=True):
-    
+
     # Initialize an empty rarity table
     rarity_table = {}
     for layer in CONFIG:
-        rarity_table[layer['name']] = []
+        rarity_table[layer["name"]] = []
 
     # Define output path to output/edition {edition_num}
-    op_path = os.path.join('output', 'edition ' + str(edition), 'images')
+    op_path = os.path.join("output", "edition " + str(edition), "images")
 
     # Will require this to name final images as 000, 001,...
     zfill_count = len(str(count - 1))
-    
+
     # Create output directory if it doesn't exist
     if not os.path.exists(op_path):
         os.makedirs(op_path)
-      
+
     # Create the images
     for n in progressbar(range(count)):
-        
+
         # Set image name
-        image_name = str(n).zfill(zfill_count) + '.png'
-        
+        image_name = str(n).zfill(zfill_count) + ".mp4"
+
         # Get a random set of valid traits based on rarity weights
         trait_sets, trait_paths = generate_trait_set_from_config()
 
         # Generate the actual image
-        generate_single_image(trait_paths, os.path.join(op_path, image_name))
-        
+        generate_single_deity(trait_paths, os.path.join(op_path, image_name))
+
         # Populate the rarity table with metadata of newly created image
         for idx, trait in enumerate(trait_sets):
             if trait is not None:
-                rarity_table[CONFIG[idx]['name']].append(trait[: -1 * len('.png')])
+                rarity_table[CONFIG[idx]["name"]].append(trait[: -1 * len(".mp4")])
             else:
-                rarity_table[CONFIG[idx]['name']].append('none')
-    
+                rarity_table[CONFIG[idx]["name"]].append("none")
+
     # Create the final rarity table by removing duplicate creat
     rarity_table = pd.DataFrame(rarity_table).drop_duplicates()
     print("Generated %i images, %i are distinct" % (count, rarity_table.shape[0]))
-    
+
     if drop_dup:
         # Get list of duplicate images
         img_tb_removed = sorted(list(set(range(count)) - set(rarity_table.index)))
@@ -190,22 +186,25 @@ def generate_images(edition, count, drop_dup=True):
         # Remove duplicate images
         print("Removing %i images..." % (len(img_tb_removed)))
 
-        #op_path = os.path.join('output', 'edition ' + str(edition))
+        # op_path = os.path.join('output', 'edition ' + str(edition))
         for i in img_tb_removed:
-            os.remove(os.path.join(op_path, str(i).zfill(zfill_count) + '.png'))
+            os.remove(os.path.join(op_path, str(i).zfill(zfill_count) + ".mp4"))
 
         # Rename images such that it is sequentialluy numbered
         for idx, img in enumerate(sorted(os.listdir(op_path))):
-            os.rename(os.path.join(op_path, img), os.path.join(op_path, str(idx).zfill(zfill_count) + '.png'))
-    
-    
+            os.rename(
+                os.path.join(op_path, img),
+                os.path.join(op_path, str(idx).zfill(zfill_count) + ".mp4"),
+            )
+
     # Modify rarity table to reflect removals
     rarity_table = rarity_table.reset_index()
-    rarity_table = rarity_table.drop('index', axis=1)
+    rarity_table = rarity_table.drop("index", axis=1)
     return rarity_table
 
+
 # Main function. Point of entry
-def main():
+def main(num_nfts, nft_name):
 
     print("Checking assets...")
     parse_config()
@@ -216,23 +215,18 @@ def main():
     print("You can create a total of %i distinct avatars" % (tot_comb))
     print()
 
-    print("How many avatars would you like to create? Enter a number greater than 0: ")
     while True:
-        num_avatars = int(input())
-        if num_avatars > 0:
+        if num_nfts > 0:
             break
-    
-    print("What would you like to call this edition?: ")
-    edition_name = input()
 
     print("Starting task...")
-    rt = generate_images(edition_name, num_avatars)
+    rt = generate_images(nft_name, num_nfts)
 
     print("Saving metadata...")
-    rt.to_csv(os.path.join('output', 'edition ' + str(edition_name), 'metadata.csv'))
+    rt.to_csv(os.path.join("output", "edition " + str(nft_name), "metadata.csv"))
 
     print("Task complete!")
 
 
 # Run the main function
-main()
+main(5, "Deity")
